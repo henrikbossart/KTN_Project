902a3576256b6bc3ce80b03dcaa4c66ea3c24c9c
Henrik <henrikbossart@me.com>
2016-02-25 14:28:06 +0100

    ...

---
+++ Java/src/Client.java
@@ -0,0 +1,6 @@
+/**
+ * Created by henrikbossart on 25.02.2016.
+ */
+
+public class Client {
+}
---
+++ Java/src/MessageReciever.java
@@ -0,0 +1,5 @@
+/**
+ * Created by henrikbossart on 25.02.2016.
+ */
+public class MessageReciever {
+}
