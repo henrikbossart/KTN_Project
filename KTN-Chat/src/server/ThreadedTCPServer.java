67a2f968e1e9a3d5e28755d591b33c9d8754ba7b
mayhelenrs <mayhelenrs@icloud.com>
2016-03-11 14:36:53 +0100

    changes ThreadedTCPServer

--- KTN-Chat/src/server/ThreadedTCPServer.java
+++ KTN-Chat/src/server/ThreadedTCPServer.java
@@ -2,6 +2,7 @@ package server;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import net.sf.json.JSONObject;
@@ -9,11 +10,13 @@ import net.sf.json.JSONObject;
 public class ThreadedTCPServer {
 	private static ServerSocket serverSocket;
 	private static HashMap<String, Socket> clients;
+	private static ArrayList<JSONObject> history;
 
 	public static void start() throws Exception {
 		// create the server
 		serverSocket = new ServerSocket(10000);
 		clients = new HashMap<String, Socket>();
+		history = new ArrayList<JSONObject>();
 
 		while (true) {
 			Socket client = serverSocket.accept();
@@ -29,8 +32,24 @@ public class ThreadedTCPServer {
 			return false;
 		}
 	}
+	
+	public static boolean removeClient(String username, Socket clientSocket) {
+		if (clients.containsKey(username)) {
+			clients.remove(username);
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	public static void broadcast(JSONObject message) {
-		// send JSONObject to all clients
+		for (String key : clients.keySet()) {
+			key.sendMessage(message);
+		}
+		addHistory(message.getString("sender"), message);
+	}
+	
+	private static void addHistory(String sender, JSONObject message) {
+		history.add(message);
 	}
 }
