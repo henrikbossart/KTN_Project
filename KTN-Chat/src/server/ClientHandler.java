c0c37da074a8c33e48234c6b2e590a6ad0f1e893
Henrik <henrikbossart@me.com>
2016-03-11 15:27:07 +0100

    ...

--- KTN-Chat/src/server/ClientHandler.java
+++ KTN-Chat/src/server/ClientHandler.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package server;
 
 import java.io.IOException;
@@ -93,4 +94,74 @@ public class ClientHandler implements Runnable {
 		message.put("content", content);
 		return message;
 	}
+=======
+package server;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
+import net.sf.json.JSONObject;
+
+public class ClientHandler implements Runnable {
+
+	private Socket client;
+
+	public ClientHandler(Socket client) {
+		this.client = client;
+	}
+
+	@Override
+	public void run() {
+		boolean connected = true;
+		while (connected) {
+			// get the incoming message
+			ObjectInputStream incoming;
+			try {
+				incoming = new ObjectInputStream(client.getInputStream());
+				JSONObject clientMessage = (JSONObject) incoming.readObject();
+
+				// act based on content of JSONObject
+				switch (clientMessage.getString("request")) {
+				case "login":
+					String username = clientMessage.getString("content");
+					if (ThreadedTCPServer.addClient(username, client)) {
+						// send reply: successful connection
+					} else {
+						// send reply: unsuccessful connection, and terminate connection
+						connected = false;
+					}
+					break;
+
+				case "logout":
+					// remove client from list of active users
+
+					// terminate connection
+					connected = false;
+					break;
+
+				case "msg":
+					// tell server to broadcast message
+
+					break;
+
+				case "names":
+					// send reply with list of user names
+					break;
+
+				case "help":
+					// send reply with help text
+					break;
+
+				default:
+					// send reply with unknown command error
+					break;
+				}
+
+			} catch (IOException | ClassNotFoundException e) {
+				e.printStackTrace();
+			} 
+		}
+	}
+>>>>>>> master
 }
--- KTN-Chat/src/server/ThreadedTCPServer.java
+++ KTN-Chat/src/server/ThreadedTCPServer.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package server;
 
 import java.net.ServerSocket;
@@ -33,4 +34,60 @@ public class ThreadedTCPServer {
 	public static void broadcast(JSONObject message) {
 		// send JSONObject to all clients
 	}
+=======
+package server;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import net.sf.json.JSONObject;
+
+public class ThreadedTCPServer {
+	private static ServerSocket serverSocket;
+	private static HashMap<String, Socket> clients;
+	private static ArrayList<JSONObject> history;
+
+	public static void start() throws Exception {
+		// create the server
+		serverSocket = new ServerSocket(10000);
+		clients = new HashMap<String, Socket>();
+		history = new ArrayList<JSONObject>();
+
+		while (true) {
+			Socket client = serverSocket.accept();
+			new Thread(new ClientHandler(client)).start();
+		}
+	}
+
+	public static boolean addClient(String username, Socket clientSocket) {
+		if (!clients.containsKey(username)) {
+			clients.put(username, clientSocket);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public static boolean removeClient(String username, Socket clientSocket) {
+		if (clients.containsKey(username)) {
+			clients.remove(username);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static void broadcast(JSONObject message) {
+		for (String key : clients.keySet()) {
+			key.sendMessage(message);
+		}
+		addHistory(message.getString("sender"), message);
+	}
+	
+	private static void addHistory(String sender, JSONObject message) {
+		history.add(message);
+	}
+>>>>>>> master
 }
