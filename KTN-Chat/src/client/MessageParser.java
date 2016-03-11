bcf376d35fe0911b4b6b5c3df6a1d6bb5b53d2d2
Henrik <henrikbossart@me.com>
2016-03-11 15:52:48 +0100

    ..

--- KTN-Chat/src/client/MessageParser.java
+++ KTN-Chat/src/client/MessageParser.java
@@ -4,6 +4,14 @@ import net.sf.json.JSONObject;
 
 public class MessageParser {
 
+    private String timestamp;
+    private String response;
+    private String content;
+    private String sender;
+
     public MessageParser(JSONObject payload) {
-        
+        this.sender = payload.getString("sender");
+        this.content = payload.getString("content");
+        this.response = payload.getString("response");
+        this.timestamp = payload.getString("timestamp");
 }
