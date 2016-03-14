11deec67c1b4dfe163a2c662b1558e6608c6d457
Silje <siljef21@hotmail.com>
2016-03-14 12:52:45 +0100

    New update MessageReciever

--- KTN-Chat/src/client/MessageReciever.java
+++ KTN-Chat/src/client/MessageReciever.java
@@ -1,12 +1,28 @@
 package client;
 
+import java.net.Socket;
+import java.util.ArrayList;
+
 import net.sf.json.JSONObject;
 
 public class MessageReciever {
-	JSONObject message;
+	ArrayList<JSONObject> messages = new ArrayList<JSONObject>();
+	Socket clientSocket;
+	
+	public MessageReciever(Socket socket){
+		this.clientSocket = socket;
+	}
 
 	public void recieve(JSONObject message) {
-		this.message = message;
+		messages.add(message);
 	}
-	// STACK av JSON objekter
+	
+	public JSONObject getMessage(){
+		if (! (messages.size() == 0)){
+			JSONObject message = messages.get(0);
+			messages.remove(0);
+			return message;
+		}
+		return null;
+	}
 }
